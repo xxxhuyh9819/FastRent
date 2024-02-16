15ad35f6966c1d148ef0e2e3d33fea2f791a3ca9
xxxhuyh9819 <145178951+xxxhuyh9819@users.noreply.github.com>
2024-02-16 14:45:55 +0800

    Uploaded the empty project

---
+++ fast-rent/fast-rent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fast-rent
+//
+//  Created by Yunhao Hu on 2024/2/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fast-rent/fast-rent/fast_rentApp.swift
@@ -0,0 +1,17 @@
+//
+//  fast_rentApp.swift
+//  fast-rent
+//
+//  Created by Yunhao Hu on 2024/2/16.
+//
+
+import SwiftUI
+
+@main
+struct fast_rentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
