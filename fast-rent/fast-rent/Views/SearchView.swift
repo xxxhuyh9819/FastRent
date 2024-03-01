1d9b625559541eb76e8554a682d70f390951a5d3
yunhao <xxxhuyh9819@gmail.com>
2024-03-01 17:14:34 -0600

    updated searchview

--- fast-rent/fast-rent/Views/SearchView.swift
+++ fast-rent/fast-rent/Views/SearchView.swift
@@ -13,6 +13,9 @@ struct SearchView: View {
     @State var isLeaving = false
     
     @EnvironmentObject var viewModel: MainViewModel
+    @FocusState private var isEnteringLocation: Bool
+    @FocusState private var isEnteringMinPrice: Bool
+    @FocusState private var isEnteringMaxPrice: Bool
     
     var locationIsEmpty: Bool {
         return viewModel.location.isEmpty
@@ -21,74 +24,78 @@ struct SearchView: View {
     var body: some View {
         
         NavigationStack {
-            VStack(alignment: .leading, spacing: 8) {
-                
-                Text("Location")
-                    .font(.title3)
-                    .fontWeight(.bold)
-                
-                VStack {
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                        TextField("Enter city", text: $viewModel.location)
-                            .font(.footnote)
+            ScrollView {
+                VStack(alignment: .leading, spacing: 8) {
+                    
+                    Text("Location")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    
+                    VStack {
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                            TextField("Enter city", text: $viewModel.location)
+                                .font(.footnote)
+                                .focused($isEnteringLocation)
+                        }
+                        .modifier(CapsuleModifier())
+                        
+                        if (locationIsEmpty) {
+                            Text("Location is required!")
+                                .font(.footnote)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .foregroundStyle(.red)
+                        }
                     }
-                    .modifier(CapsuleModifier())
+                }
+                .modifier(SearchSectionModifier())
+                .padding()
+                
+                VStack(alignment: .leading, spacing: 8) {
                     
-                    if (locationIsEmpty) {
-                        Text("Location is required!")
-                            .font(.footnote)
-                            .frame(maxWidth: .infinity, alignment: .trailing)
-                            .foregroundStyle(.red)
+                    Text("Price")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    
+                    HStack(spacing: 10) {
+                        TextField("No Min", text: $viewModel.minPrice)
+                            .focused($isEnteringMinPrice)
+                            .keyboardType(.numberPad)
+                            .modifier(CapsuleModifier())
+
+                        
+                        Rectangle()
+                            .frame(width: 12, height: 1)
+                            .foregroundStyle(.gray)
+                        
+                        TextField("No Max", text: $viewModel.maxPrice)
+                            .keyboardType(.numberPad)
+                            .modifier(CapsuleModifier())
                     }
+                    .font(.footnote)
                 }
-            }
-            .modifier(SearchSectionModifier())
-            .padding()
-            
-            VStack(alignment: .leading, spacing: 8) {
+                .modifier(SearchSectionModifier())
+                .padding()
                 
-                Text("Price")
-                    .font(.title3)
-                    .fontWeight(.bold)
-                
-                HStack(spacing: 10) {
-                    TextField("No Min", text: $viewModel.minPrice)
-                        .keyboardType(.numberPad)
-                        .modifier(CapsuleModifier())
+                VStack(alignment: .leading, spacing: 8) {
                     
-                    Rectangle()
-                        .frame(width: 12, height: 1)
-                        .foregroundStyle(.gray)
+                    Text("Beds & Baths")
+                        .font(.title3)
+                        .fontWeight(.bold)
                     
-                    TextField("No Max", text: $viewModel.maxPrice)
-                        .keyboardType(.numberPad)
-                        .modifier(CapsuleModifier())
+                    VStack(alignment: .leading, spacing: 16) {
+                        CustomStepper(number: $viewModel.numBedrooms, title: "Bedrooms")
+                            .modifier(CapsuleModifier())
+                        
+                        CustomStepper(number: $viewModel.numBathrooms, title: "Bathrooms")
+                            .modifier(CapsuleModifier())
+                    }
+                    .font(.footnote)
                 }
-                .font(.footnote)
+                .modifier(SearchSectionModifier())
+                .padding()
             }
-            .modifier(SearchSectionModifier())
-            .padding()
-            
-            
-            VStack(alignment: .leading, spacing: 8) {
-                
-                Text("Beds & Baths")
-                    .font(.title3)
-                    .fontWeight(.bold)
-                
-                VStack(alignment: .leading, spacing: 16) {
-                    CustomStepper(number: $viewModel.numBedrooms, title: "Bedrooms")
-                        .modifier(CapsuleModifier())
-                    
-                    CustomStepper(number: $viewModel.numBathrooms, title: "Bathrooms")
-                        .modifier(CapsuleModifier())
-                }
-                .font(.footnote)
-            }
-            .modifier(SearchSectionModifier())
-            .padding()
-            
+            .ignoresSafeArea(.keyboard, edges: .bottom)
             .navigationTitle("Search")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
